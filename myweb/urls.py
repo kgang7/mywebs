6117291a74c3ba9169f8f53396f1f7c07da74ada
Mmdkian <shahriarii.kian@gmail.com>
2025-02-26 18:03:41 +0330

    heloooo

--- myweb/urls.py
+++ myweb/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myweb.view import http_test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('http_test')
 ]
---
+++ myweb/view.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+
+def http_test(request):
+    return HttpResponse("abas")
+
