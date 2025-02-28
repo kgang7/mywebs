4f711f0001ed23f3fa2cd8e5f9c05c7d8b56d3a3
Mmdkian <shahriarii.kian@gmail.com>
2025-02-28 17:19:49 +0330

    rr

--- myweb/urls.py
+++ myweb/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from mywebs.view import http_test
-from mywebs.view import json_test
+from myweb.view.http_test import http_test
+from myweb.view.json_test import json_test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('http_test', http_test),
-    path('json_test', json_test)
+    path('json_test', json_test),
     
 ]
+
 
 
--- myweb/view.py
+++ myweb/view.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
+from django.json import JsonResponse
+
 
 
 def http_test(request):
-    return HttpResponse("abas")
+    return HttpResponse("drood")
+
+def json_test(request):
+    return JsonResponse("abas")
 
