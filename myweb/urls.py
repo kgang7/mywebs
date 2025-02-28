c75d73d01d9140a5dfe6a673f56699391cea7dad
Mmdkian <shahriarii.kian@gmail.com>
2025-02-28 10:25:27 +0330

    Update urls.py

--- myweb/urls.py
+++ myweb/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from myweb.view import http_test
+from mywebs.view import http_test
+from mywebs.view import json_test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('http_test', http_test)
+    path('http_test', http_test),
+    path('json_test', json_test)
     
 ]
 
