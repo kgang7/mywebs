f5e0f4a29e0e82c25b0d113ecaf2af525a0ed6ac
Mmdkian <shahriarii.kian@gmail.com>
2025-03-10 18:59:03 +0330

    abas sosis nadare

--- website/url.py
+++ website/url.py
@@ -3,7 +3,7 @@ from django.urls import path
 from website.views import *
 
 urlpatterns = [
-   path('' ,index_views),
+   path('home' ,index_views),
    path('about' ,about_views),
    path('contact' ,contact_views),
 ]
--- website/views.py
+++ website/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from django.template import Template
+
 
 
 def index_views(request):
-    return HttpResponse("<h1>home page</h1>")
+    return render(request, 'websites/index.html')
 
 
 def about_views(request):
-    return HttpResponse("<h1>about</h1>")
+    return render(request, 'websites/about.html')
 
 
 def contact_views(request):
-    return HttpResponse("<h1>contact</h1>")
+    return render(request, 'websites/contact.html')
