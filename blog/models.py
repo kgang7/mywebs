9013a50a8c82a48228996ad10e148592e4c53afe
kian <shahriarii.kian@gmail.com>
2025-08-25 18:40:10 +0330

    add a new model

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length= 250)
     content = models.TextField()
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=True)
+    publlished_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
     
