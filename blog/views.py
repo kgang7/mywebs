e20ce63222305a45bd3d0db5cd3c6a2bfffede6d
kian <shahriarii.kian@gmail.com>
2025-10-30 17:05:45 +0330

    jjkkj

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.object.filter(status=1)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html',context)
 
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
+
+def test(request):
+    posts = Post.object.all()
+    context = {'posts':posts}
+    return render(request,'test.html', context)
 
--- website/views.py
+++ website/views.py
@@ -16,4 +16,6 @@ def contact_views(request):
     return render(request, 'mysite/contact.html')
 
 def test(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
     return render(request, 'test.html')
